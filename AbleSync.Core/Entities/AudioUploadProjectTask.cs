2869482483a27d26fe234ac36ee38f3155b59231
Thomas Beckers <tabeckers95@gmail.com>
2020-10-03 14:08:35 +0200

    Created project task entities

---
+++ AbleSync.Core/Entities/AudioUploadProjectTask.cs
@@ -0,0 +1,13 @@
+﻿namespace AbleSync.Core.Entities
+{
+    /// <summary>
+    ///     <see cref="ProjectTask"/> representing the upload of an audio file.
+    /// </summary>
+    public sealed class AudioUploadProjectTask : ProjectTask
+    {
+        /// <summary>
+        ///     The file name of the file to upload.
+        /// </summary>
+        public string FileName { get; set; }
+    }
+}
---
+++ AbleSync.Core/Entities/ProjectTask.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace AbleSync.Core.Entities
+{
+    /// <summary>
+    ///     Abstract base class that represents a processing task for a given
+    ///     <see cref="Project"/>.
+    /// </summary>
+    public abstract class ProjectTask : EntityBase, IEntityAudit
+    {
+        /// <summary>
+        ///     The referenced <see cref="Project"/> to which this task belongs.
+        /// </summary>
+        public Guid ProjectId { get; set; }
+
+        /// <summary>
+        ///     Represents whether or not this item has been completed.
+        /// </summary>
+        public bool Completed { get; set; }
+
+        /// <summary>
+        ///     Creation time.
+        /// </summary>
+        public DateTimeOffset? DateCreated { get; set; }
+
+        /// <summary>
+        ///     Latest update time.
+        /// </summary>
+        public DateTimeOffset? DateUpdated { get; set; }
+
+        /// <summary>
+        ///     Task completion time.
+        /// </summary>
+        public DateTimeOffset? DateCompleted { get; set; }
+    }
+}
