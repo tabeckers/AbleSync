8b8cfbe182ce88de24447abb2401e3359d1dadc8
Thomas Beckers <tabeckers95@gmail.com>
2020-10-03 17:13:10 +0200

    Created new exceptions

---
+++ AbleSync.Core/Exceptions/NotAnAbletonProjectFolderException.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace AbleSync.Core.Exceptions
+{
+    /// <summary>
+    ///     Indicates we try to process a directory which is not an Ableton
+    ///     project directory as if it were one.
+    /// </summary>
+    public sealed class NotAnAbletonProjectFolderException : AbleSyncBaseException
+    {
+        public NotAnAbletonProjectFolderException()
+        {
+        }
+
+        public NotAnAbletonProjectFolderException(string message) : base(message)
+        {
+        }
+
+        public NotAnAbletonProjectFolderException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        public NotAnAbletonProjectFolderException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
