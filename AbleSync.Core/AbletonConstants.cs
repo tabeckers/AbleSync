69eed5e7f4b862078bf88db9266a7200d3c652c4
Thomas Beckers <tabeckers95@gmail.com>
2020-10-03 14:43:16 +0200

    Created Ableton project folder recognition #5

---
+++ AbleSync.Core/AbletonConstants.cs
@@ -0,0 +1,38 @@
+﻿namespace AbleSync.Core
+{
+    /// <summary>
+    ///     Contains application global constant values with regards to Ableton.
+    /// </summary>
+    public static class AbletonConstants
+    {
+        /// <summary>
+        ///     The file extension of an Ableton project file.
+        /// </summary>
+        public const string ProjectFileExtension = ".als";
+
+        /// <summary>
+        ///     Name of the folder which contains the Ableton project info.
+        /// </summary>
+        public const string ProjectInfoDirectoryName = "Ableton Project Info";
+
+        /// <summary>
+        ///     File name of the project info icon file.
+        /// </summary>
+        public const string ProjectInfoIconFileName = "AProject";
+
+        /// <summary>
+        ///     File extension of the project info icon file.
+        /// </summary>
+        public const string ProjectInfoIconFileExtension = ".ico";
+
+        /// <summary>
+        ///     File name of the project info file.
+        /// </summary>
+        public const string ProjectInfoFileName = "Project8_1";
+
+        /// <summary>
+        ///     File extension of the project info file.
+        /// </summary>
+        public const string ProjectInfoFileExtension = ".cfg";
+    }
+}
---
+++ AbleSync.Core/Helpers/ProjectFolderHelper.cs
@@ -0,0 +1,89 @@
+﻿using AbleSync.Core.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AbleSync.Core.Helpers
+{
+    // TODO This is Windows specific at the moment.
+    // TODO This should look at file/contenttype instead of extensions.
+    /// <summary>
+    ///     Helper class for recognizing Ableton project folders.
+    /// </summary>
+    public static class ProjectFolderHelper
+    {
+        /// <summary>
+        ///     Checks if a directory is an Ableton project directory.
+        /// </summary>
+        /// <param name="directoryInfo">The directory to check.</param>
+        /// <returns><c>true</c> if the directory is an Ableton project directory.</returns>
+        public static bool IsAbletonProjectFolder(DirectoryInfo directoryInfo)
+        {
+            if (directoryInfo == null)
+            {
+                throw new ArgumentNullException(nameof(directoryInfo));
+            }
+
+            try
+            {
+                var subDirectories = directoryInfo.GetDirectories();
+                var files = directoryInfo.GetFiles();
+
+                // Check for an actual Ableton project file.
+                if (!files.Where(x => x.Extension == AbletonConstants.ProjectFileExtension).Any())
+                {
+                    return false;
+                }
+
+                // Check for the project info directory.
+                if (!DoesDirectoryContainDirectory(subDirectories, AbletonConstants.ProjectInfoDirectoryName))
+                {
+                    return false;
+                }
+                else
+                {
+                    var projectInfoDirectory = subDirectories.Where(x => x.Name == AbletonConstants.ProjectInfoDirectoryName).First();
+                    if (!DoesDirectoryContainFile(projectInfoDirectory, AbletonConstants.ProjectInfoFileName, AbletonConstants.ProjectInfoFileExtension) ||
+                        !DoesDirectoryContainFile(projectInfoDirectory, AbletonConstants.ProjectInfoIconFileName, AbletonConstants.ProjectInfoIconFileExtension))
+                    {
+                        return false;
+                    }
+                }
+
+                // If we have passed all checks the directory must be a project folder.
+                return true;
+            }
+            catch (IOException e)
+            {
+                throw new FileAccessException("Could not determine if directory was Ableton project", e);
+            }
+        }
+
+        /// <summary>
+        ///     Checks if a collection of directories contains a specific directory
+        ///     with a given name.
+        /// </summary>
+        /// <remarks>
+        ///     This does not check for any duplicates.
+        /// </remarks>
+        /// <param name="subDirectories">The directory collection.</param>
+        /// <param name="directoryName">The directory name to check for.</param>
+        /// <returns><c>true</c> if it exists in the collection.</returns>
+        private static bool DoesDirectoryContainDirectory(IEnumerable<DirectoryInfo> subDirectories, string directoryName)
+            => subDirectories.Where(x => x.Name == directoryName).Any();
+
+        /// <summary>
+        ///     Checks if a given directory contains a specific file.
+        /// </summary>
+        /// <remarks>
+        ///     This does not check for any duplicates.
+        /// </remarks>
+        /// <param name="directory">The directory to check.</param>
+        /// <param name="fileName">The file name to check.</param>
+        /// <param name="fileExtension">The file extension to check.</param>
+        /// <returns><c>true</c> if the directory contains the file.</returns>
+        private static bool DoesDirectoryContainFile(DirectoryInfo directory, string fileName, string fileExtension)
+            => directory.GetFiles().Where(x => x.Name == $"{fileName}{fileExtension}").Any();
+    }
+}
