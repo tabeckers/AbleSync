35051648c905699c5681630b154a24033f5e31e3
Thomas Beckers <tabeckers95@gmail.com>
2020-10-04 15:37:07 +0200

    Create AbleSyncOptions.cs

---
+++ AbleSync.Core/AbleSyncOptions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace AbleSync.Core
+{
+    /// <summary>
+    ///     Options file for the global application.
+    /// </summary>
+    public sealed class AbleSyncOptions
+    {
+        /// <summary>
+        ///     The root directory.
+        /// </summary>
+        public Uri RootDirectoryPath { get; set; }
+    }
+}
