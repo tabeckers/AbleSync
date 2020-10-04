afc92adc378b852fb346aeb3d26ce981e628cf91
Thomas Beckers <tabeckers95@gmail.com>
2020-10-04 17:33:53 +0200

    Added project task types and entity updates
    We no longer have explicit objects for a project tasks, just an enum indicating the type

---
+++ AbleSync.Core/Types/ProjectTaskStatus.cs
@@ -0,0 +1,28 @@
+﻿namespace AbleSync.Core.Types
+{
+    /// <summary>
+    ///     Enum representing the status of a project task.
+    /// </summary>
+    public enum ProjectTaskStatus
+    {
+        /// <summary>
+        ///     The task has been created.
+        /// </summary>
+        Created,
+
+        /// <summary>
+        ///     Some entity is processing the task.
+        /// </summary>
+        Processing,
+
+        /// <summary>
+        ///     The task has been processed successfully.
+        /// </summary>
+        Done,
+
+        /// <summary>
+        ///     The task has failed.
+        /// </summary>
+        Failed
+    }
+}
