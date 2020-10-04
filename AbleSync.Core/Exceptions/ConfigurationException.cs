3915db02b4882a7f8c4622905f94dc55555f497e
Thomas Beckers <tabeckers95@gmail.com>
2020-10-04 15:36:23 +0200

    Create ConfigurationException.cs

---
+++ AbleSync.Core/Exceptions/ConfigurationException.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace AbleSync.Core.Exceptions
+{
+    /// <summary>
+    ///     Indicates our configuration is incorrect.
+    /// </summary>
+    public sealed class ConfigurationException : AbleSyncBaseException
+    {
+        public ConfigurationException()
+        {
+        }
+
+        public ConfigurationException(string message) : base(message)
+        {
+        }
+
+        public ConfigurationException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        public ConfigurationException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
