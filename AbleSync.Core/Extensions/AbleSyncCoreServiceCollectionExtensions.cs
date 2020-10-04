e2329e3eef167acddd5ff0c5edc992b382cd0116
Thomas Beckers <tabeckers95@gmail.com>
2020-10-04 15:37:03 +0200

    Create AbleSyncCoreServiceCollectionExtensions.cs

---
+++ AbleSync.Core/Extensions/AbleSyncCoreServiceCollectionExtensions.cs
@@ -0,0 +1,32 @@
+﻿using AbleSync.Core.Interfaces.Services;
+using AbleSync.Core.Services;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace AbleSync.Core.Extensions
+{
+    /// <summary>
+    ///     Extensions functionality for <see cref="IServiceCollection"/>.
+    /// </summary>
+    public static class AbleSyncCoreServiceCollectionExtensions
+    {
+        /// <summary>
+        ///     Adds all our infrastructure services to the container.
+        /// </summary>
+        /// <param name="services">The <see cref="IServiceCollection"/> to add the services to.</param>
+        /// <returns>Same as <paramref name="services"/>.</returns>
+        public static IServiceCollection AddAbleSyncCoreServices(this IServiceCollection services)
+        {
+            if (services == null)
+            {
+                throw new ArgumentNullException(nameof(services));
+            }
+
+            // Add services.
+            services.AddScoped<IProjectScrapingService, ProjectScrapingService>();
+            services.AddScoped<IFileTrackingService, FileTrackingService>();
+
+            return services;
+        }
+    }
+}
