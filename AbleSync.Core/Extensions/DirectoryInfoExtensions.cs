422a56a60daf2deef0bdbc7c6f415aef1252bf93
Thomas Beckers <tabeckers95@gmail.com>
2020-10-25 14:03:45 +0100

    Further fixed path formatting

---
+++ AbleSync.Core/Extensions/DirectoryInfoExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace AbleSync.Core.Extensions
+{
+    /// <summary>
+    ///     Extension functionality for <see cref="DirectoryInfo"/>.
+    /// </summary>
+    public static class DirectoryInfoExtensions
+    {
+        /// <summary>
+        ///     Prints a directory info path in the desired format.
+        /// </summary>
+        /// <param name="self">The directory info.</param>
+        /// <returns>The formatted path string.</returns>
+        public static string FullPathFormatted(this DirectoryInfo self)
+        {
+            if (self == null)
+            {
+                throw new ArgumentNullException(nameof(self));
+            }
+
+            return self.FullName.Replace("\\", "/", StringComparison.InvariantCulture);
+        }
+    }
+}
