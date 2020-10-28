e432017d3865c7fb6af0850921f7fd5ced7370a0
Thomas Beckers <tabeckers95@gmail.com>
2020-10-28 11:04:39 +0100

    Create FileNameHelper.cs

---
+++ AbleSync.Core/Helpers/FileNameHelper.cs
@@ -0,0 +1,27 @@
+﻿using AbleSync.Core.Types;
+using System;
+
+namespace AbleSync.Core.Helpers
+{
+    // TODO Bad name.
+    /// <summary>
+    ///     Contains generic file name mapping functionality.
+    /// </summary>
+    public static class FileNameHelper
+    {
+        /// <summary>
+        ///     Translates an audio format to the corresponding
+        ///     file extension.
+        /// </summary>
+        /// <param name="audioFormat">The audio format.</param>
+        /// <returns>The file extension, including the '.' dot.</returns>
+        public static string ToExtension(AudioFormat audioFormat)
+            => audioFormat switch
+            {
+                AudioFormat.Mp3 => Constants.AudioMp3FileExtension,
+                AudioFormat.Wav => Constants.AudioWavFileExtension,
+                AudioFormat.Flac => Constants.AudioFlacFileExtension,
+                _ => throw new InvalidOperationException(nameof(audioFormat))
+            };
+    }
+}
